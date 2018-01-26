6daf177a4722e7bc5057ed0cd9aa0830ac16b348
aoyumei <13203864306@163.com>
2018-01-26 14:31:34 +0800

    测试提交

---
+++ workManage/src/main/java/com/aym/controller/test.java
@@ -0,0 +1,15 @@
+package com.aym.controller;
+
+/**
+ * Created by task on 2018/1/26.
+ */
+public class test {
+
+
+    public static void main(String[] args) {
+        System.out.println("@@@@@");
+    }
+
+
+
+}
